d986573359f23a9ae84ad33d93159032922f5008
unknown <mostafa.h.gh.1377@gmial.com>
2020-11-03 14:43:47 +0100

    update posts

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import Link from 'next/link'
-import { connect } from 'react-redux'
-import Layout from '../components/Layout';
-import Post from '../components/Post';
-
+import Link from "next/link";
+import { connect } from "react-redux";
+import Layout from "../components/Layout";
+import Post from "../components/Post";
 
 function Home(props) {
   console.log(props);
@@ -13,16 +12,9 @@ function Home(props) {
         <Post />
         <Post />
         <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
       </main>
     </Layout>
-  )
+  );
 }
 
-
-export default connect(state => state)(Home)
+export default connect((state) => state)(Home);
